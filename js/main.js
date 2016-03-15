e2b83ff57cd87689cd082889568458988544d8e4
Chan0369 <xbetty_chanx@outlook.com>
2016-03-14 23:09:45 -0400

    Added images

--- js/main.js
+++ js/main.js
@@ -1,13 +1,17 @@
 var $firstHeading = $('<h2>');
-var $firstImage = $('<img>');
+var $firstImage = $('<img src="images/peanuts.jpg" alt="">');
 var $firstParagraph = $('<p>');
 var $li = $('<li>');
 var $secondHeading = $('<h2>');
-var $secondImage = $('<img>');
+var $secondImage = $('<img src="images/beans.jpg" alt="">');
 var $secondParagraph = $('<p>');
 var $thirdHeading = $('<h2>');
-var $thirdImage = $('<img>');
+var $thirdImage = $('<img src="images/lentils.jpg" alt="">');
 var $thirdParagraph = $('<p>');
+
+$firstImage.html('peanuts.jpg');
+$('body').append($firstImage);
+$('body').append($li);
 
 $firstHeading.html('Peanuts');
 $('body').append($firstHeading);
@@ -17,12 +21,20 @@ $firstParagraph.html('First cultivated in the valleys of Paraguay.');
 $('body').append($firstParagraph);
 $('body').append($li);
 
+$secondImage.html('beans.jpg');
+$('body').append($secondImage);
+$('body').append($li);
+
 $secondHeading.html('Beans');
 $('body').append($secondHeading);
 $('body').append($li);
 
 $secondParagraph.html('A summer crop that needs warm temperatures.');
 $('body').append($secondParagraph);
+$('body').append($li);
+
+$thirdImage.html('lentils.jpg');
+$('body').append($thirdImage);
 $('body').append($li);
 
 $thirdHeading.html('Lentils');
