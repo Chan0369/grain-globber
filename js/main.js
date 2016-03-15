dea6dad1d6ec3a8f8ae4d30a9c2c448d3b06580b
Chan0369 <xbetty_chanx@outlook.com>
2016-03-14 22:52:43 -0400

    Added variables and added h2

---
+++ js/main.js
@@ -0,0 +1,20 @@
+var $firstHeading = $('<h2>');
+var $firstImage = $('<img>');
+var $paragraph = $('<p>');
+var $li = $('<li>');
+var $secondHeading = $('<h2>');
+var $secondImage = $('<img>');
+var $thirdHeading = $('<h2>');
+var $thirdImage = $('<img>');
+
+$firstHeading.html('Peanuts');
+$('body').append($firstHeading);
+$('body').append($li);
+
+$secondHeading.html('Beans');
+$('body').append($secondHeading);
+$('body').append($li);
+
+$thirdHeading.html('Lentils');
+$('body').append($thirdHeading);
+$('body').append($li);
