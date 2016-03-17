707ba58c543b25c6e6a29df9b94b0dfb306c1d08
Chan0369 <xbetty_chanx@outlook.com>
2016-03-17 17:42:51 -0400

    Changed ul to li

--- js/main.js
+++ js/main.js
@@ -1,46 +1,18 @@
-var $firstHeading = $('<h2>');
-var $firstImage = $('<img src="images/peanuts.jpg" alt="">');
-var $firstParagraph = $('<p>');
-var $li = $('<li>');
-var $secondHeading = $('<h2>');
-var $secondImage = $('<img src="images/beans.jpg" alt="">');
-var $secondParagraph = $('<p>');
-var $thirdHeading = $('<h2>');
-var $thirdImage = $('<img src="images/lentils.jpg" alt="">');
-var $thirdParagraph = $('<p>');
+var $ul = $('ul');
 
-$firstImage.html('peanuts.jpg');
-$('body').append($firstImage);
-$('body').append($li);
+$ul.addClass('grains');
 
-$firstHeading.html('Peanuts');
-$('body').append($firstHeading);
-$('body').append($li);
+grains.forEach(function (grain) {
+  var $li = $('<li>');
+  var $h2 = $('<h2>');
+  var $img = $('<img>');
+  var $p = $('<p>');
 
-$firstParagraph.html('First cultivated in the valleys of Paraguay.');
-$('body').append($firstParagraph);
-$('body').append($li);
+  $img.attr('src', 'images/' + grain.img);
+  $h2.html(grain.name);
+  $p.html(grain.desc);
 
-$secondImage.html('beans.jpg');
-$('body').append($secondImage);
-$('body').append($li);
-
-$secondHeading.html('Beans');
-$('body').append($secondHeading);
-$('body').append($li);
-
-$secondParagraph.html('A summer crop that needs warm temperatures.');
-$('body').append($secondParagraph);
-$('body').append($li);
-
-$thirdImage.html('lentils.jpg');
-$('body').append($thirdImage);
-$('body').append($li);
-
-$thirdHeading.html('Lentils');
-$('body').append($thirdHeading);
-$('body').append($li);
-
-$thirdParagraph.html('An edible pulse from a bushy annual plant.');
-$('body').append($thirdParagraph);
-$('body').append($li);
+  $ul.append($li);
+  $li.append($img, $h2, $p);
+  $p.prepend($h2);
+});
